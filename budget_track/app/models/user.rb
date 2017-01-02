047f0bc338145bcfc98c66db11fabc378a6b9e02
Joel Garrett <jsgarrett1@gmail.com>
2017-01-02 10:27:37 -0600

    tweeking devise

--- budget_track/app/models/user.rb
+++ budget_track/app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_validation :authenticate_user!
 end
